f731c673060e3f1f7294a139b7cff2d882c1e7d9
joaobrum108 <joaobrum108gmail.com>
2025-08-18 14:51:13 -0300

    novas imagens para o carrosel

--- src/components/homepage/RedFoxCarrosel5.vue
+++ src/components/homepage/RedFoxCarrosel5.vue
@@ -2,18 +2,42 @@
 
 <template>
   <v-main>
-    <v-carousel hide-delimiters height="auto" max-height="80vh">
+    <v-carousel hide-delimiters height="auto">
       <v-carousel-item>
-        <v-img :src="bannerDisney" cover height="100%"></v-img>
+        <v-img :src="max" cover height="auto"></v-img>
       </v-carousel-item>
       <v-carousel-item>
-        <v-img :src="bannerMax" cover height="65vh"></v-img>
+        <v-img :src="deezer" cover height="auto"></v-img>
+      </v-carousel-item>
+      <v-carousel-item>
+        <v-img :src="sky" cover height="auto"></v-img>
+      </v-carousel-item>
+      <v-carousel-item>
+        <v-img :src="playkids" cover height="auto"></v-img>
+      </v-carousel-item>
+      <v-carousel-item>
+        <v-img :src="exitlag" cover height="auto"></v-img>
+      </v-carousel-item>
+      <v-carousel-item>
+        <v-img :src="itTv" cover height="auto"></v-img>
+      </v-carousel-item>
+      <v-carousel-item>
+        <v-img :src="looke" cover height="auto"></v-img>
+      </v-carousel-item>
+      <v-carousel-item>
+        <v-img :src="disney" cover height="auto"></v-img>
       </v-carousel-item>
     </v-carousel>
   </v-main>
 </template>
 
 <script setup>
-import bannerDisney from "../../assets/bannerDisney.webp";
-import bannerMax from "../../assets/maxbanner.webp";
+import max from "../../assets/max.webp";
+import deezer from "../../assets/deezer.webp";
+import sky from "../../assets/sky.webp";
+import playkids from "../../assets/plpaykids.webp";
+import exitlag from "../../assets/exitlag.webp";
+import itTv from "../../assets/itTvv.webp";
+import looke from "../../assets/looke.webp";
+import disney from "../../assets/disney.webp";
 </script>
